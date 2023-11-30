69e6e44d439f304d0c44cb79c8cd4340a50c602d
amekhail733 <111470396+amekhail733@users.noreply.github.com>
2023-11-30 12:39:06 -0700

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -31,7 +31,7 @@ const Login = () => {
         e.preventDefault();
         try {
             const { data } = await axios.post(
-                "http://localhost:4000/login",
+                "https://auth-backend-89jk.onrender.com",
                 {
                     ...inputValue,
                 },
